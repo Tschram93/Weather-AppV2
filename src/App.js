01a40b28dc86e91cae9f61403635d4d75f8f2e3d
TS93 <tschram93@yahoo.com>
2022-04-24 23:15:09 -0400

    updated dotenv; api key and url are functioning

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App(){
-  const weatherApi = `${process.env.REACT_APP_WEATHER_UR}{city name},{state code}&limit={limit}&appid=${process.env.REACT_APP_API_KEY}`;
-  console.log(process.env)
+  const weatherApi = `${process.env.REACT_APP_WEATHER_URL}=detroit,michigan${process.env.REACT_APP_API_KEY}`;
+  console.log(weatherApi);
   return (
     <div className="App">
 
