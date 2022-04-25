eea65d507c9053222ea30ca0d1c0ed8fdcc5e1d4
TS93 <tschram93@yahoo.com>
2022-04-24 22:58:52 -0400

    testing api key function from within dotenv file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App(){
-  
+  const weatherApi = `${process.env.REACT_APP_WEATHER_UR}{city name},{state code}&limit={limit}&appid=${process.env.REACT_APP_API_KEY}`;
+  console.log(process.env)
   return (
     <div className="App">
 
